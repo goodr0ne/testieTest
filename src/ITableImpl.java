2a00f0c0dd709926862e9851b9e2605bbfbc44c8
Yegor Scherbatkin <goodronechosen@gmail.com>
2019-02-14 14:59:17 +0300

    Add test launcher

--- src/ITableImpl.java
+++ src/ITableImpl.java
@@ -1,19 +1,17 @@
-import java.util.HashMap;
-
 public class ITableImpl implements ITable {
   final private int MAX_COLUMNS = 1000;
   final private int MAX_ROWS = 1000;
   final private int MAX_CELLS_COUNT = 1000;
   final private int MAX_CELL_VALUE = 1000000000;
   private int cellsCount = 0;
-  private long[][] cellTable;
-  private HashMap<Cell, Long> cellMap;
+  private long[][] cellTable = new long[MAX_COLUMNS][MAX_ROWS];
+  //private HashMap<Cell, Long> cellMap;
 
   ITableImpl() {
     for (int i = 0; i < MAX_COLUMNS; i++) {
       for (int j = 0; j < MAX_ROWS; j++) {
         Cell cell = new Cell(i, j);
-        cellMap.put(cell, new Long(0));
+        //cellMap.put(cell, new Long(0));
         cellTable[i][j] = 0;
       }
     }
@@ -22,7 +20,7 @@ public class ITableImpl implements ITable {
   @Override
   public void set(Cell cell, long value) {
     if ((cellsCount < MAX_CELLS_COUNT) && (value > 0) && (value < MAX_CELL_VALUE)) {
-      cellMap.put(cell, value);
+      //cellMap.put(cell, value);
       cellTable[cell.getColumn()][cell.getRow()] = value;
       cellsCount++;
     }
@@ -30,7 +28,7 @@ public class ITableImpl implements ITable {
 
   @Override
   public long get(Cell cell) {
-    long storedCellValue = cellMap.get(cell);
+    //long storedCellValue = cellMap.get(cell);
     return cellTable[cell.getColumn()][cell.getRow()];
   }
 
@@ -60,8 +58,11 @@ public class ITableImpl implements ITable {
     int finishRow = toCell.getRow();
     for (int i = startColumn; i < (finishColumn + 1); i++) {
       for (int j = startRow; j < (finishRow + 1); j++) {
-        sum = sum + cellTable[i][j];
-        count++;
+        long value = cellTable[i][j];
+        if (value > 0) {
+          sum = sum + cellTable[i][j];
+          count++;
+        }
       }
     }
     return sum/count;
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    ITableImpl table = new ITableImpl();
+    table.set(new Cell(1, 1), 50);
+    table.set(new Cell(3, 3), 25);
+    System.out.println("sum - " + table.sum(new Cell(), new Cell(100, 100)));
+    System.out.println("avg - " + table.avg(new Cell(), new Cell(100, 100)));
+  }
 }
