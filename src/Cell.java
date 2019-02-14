4e1d47d33db26dff87b84e9859d0a38c196bff75
Yegor Scherbatkin <goodronechosen@gmail.com>
2019-02-14 14:40:54 +0300

    Add cells map

--- src/Cell.java
+++ src/Cell.java
@@ -1,15 +1,29 @@
 public class Cell {
   private int column;
   private int row;
+
+  Cell() {
+    column = 0;
+    row = 0;
+  }
+
+  Cell(int column, int row) {
+    this.column = column;
+    this.row = row;
+  }
+
   public int getColumn() {
     return column;
   }
+
   public void setColumn(int column) {
     this.column = column;
   }
+
   public int getRow() {
     return row;
   }
+
   public void setRow(int row) {
     this.row = row;
   }
--- src/ITableImpl.java
+++ src/ITableImpl.java
@@ -1,4 +1,19 @@
+import java.util.HashMap;
+
 public class ITableImpl implements ITable {
+  final private int MAX_COLUMNS = 1000;
+  final private int MAX_ROWS = 1000;
+  final private int MAX_CELL_VALUE = 1000000000;
+  private HashMap<Cell, Long> cellMap;
+
+  ITableImpl() {
+    for (int i = 0; i < MAX_COLUMNS; i++) {
+      for (int j = 0; j < MAX_ROWS; j++) {
+        Cell cell = new Cell(i, j);
+        cellMap.put(cell, new Long(0));
+      }
+    }
+  }
 
   @Override
   public void set(Cell cell, long value) {
