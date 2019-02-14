706157899a8b2746afe3df5243448d764cad6e15
Yegor Scherbatkin <goodronechosen@gmail.com>
2019-02-14 14:28:44 +0300

    Initial commit

---
+++ src/Cell.java
@@ -0,0 +1,16 @@
+public class Cell {
+  private int column;
+  private int row;
+  public int getColumn() {
+    return column;
+  }
+  public void setColumn(int column) {
+    this.column = column;
+  }
+  public int getRow() {
+    return row;
+  }
+  public void setRow(int row) {
+    this.row = row;
+  }
+}
---
+++ src/ITableImpl.java
@@ -0,0 +1,22 @@
+public class ITableImpl implements ITable {
+
+  @Override
+  public void set(Cell cell, long value) {
+
+  }
+
+  @Override
+  public long get(Cell cell) {
+    return 0;
+  }
+
+  @Override
+  public long sum(Cell fromCell, Cell toCell) {
+    return 0;
+  }
+
+  @Override
+  public long avg(Cell fromCell, Cell toCell) {
+    return 0;
+  }
+}
